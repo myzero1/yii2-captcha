07e58f0e8773cc6f5a4cecfd955dcbec5385f744
qinxuanwu <917595913@qq.com>
2017-11-27 16:53:42 +0800

    holdon

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -3,6 +3,7 @@
 namespace myzero1\captcha\controllers;
 
 use yii\web\Controller;
+use Gregwar\Captcha\CaptchaBuilder;
 
 /**
  * Default controller for the `captcha` module
@@ -16,5 +17,18 @@ class DefaultController extends Controller
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionCaptcha()
+    {
+		$builder = new CaptchaBuilder;
+		$builder->build();
+
+		header('Content-type: image/jpeg');
+		$builder->output();
     }
 }
